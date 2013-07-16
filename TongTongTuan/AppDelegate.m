d34f30c0526c2a124173c88aae53ee4ed4779b41
JiangHu826 <410139419@qq.com>
2013-07-16 14:59:21 +0800

    添加LHNavigationController

--- TongTongTuan/AppDelegate.m
+++ TongTongTuan/AppDelegate.m
@@ -8,16 +8,14 @@
 
 #import "AppDelegate.h"
 
-#import "ViewController.h"
-
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
-    self.viewController = [[ViewController alloc] initWithNibName:@"ViewController" bundle:nil];
-    self.window.rootViewController = self.viewController;
+   
+    self.window.rootViewController = nil;
     [self.window makeKeyAndVisible];
     return YES;
 }
